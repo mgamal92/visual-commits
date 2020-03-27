6f36f7edceb7fdafbcdf9f44d6ef227d8cd08c31
Mohamed Gamal <mg.dev1992@gmail.com>
2020-03-27 23:50:20 +0200

    list directories in specific directory

--- main.go
+++ main.go
@@ -1,11 +1,52 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
 // scan given a path crawls it and its subfolders
 // searching for Git repositories
-func scan(path string) {
-	print("scan")
+func scan(folder string) {
+	fmt.Printf("\nFound Folders\n")
+	recursiveScanGitFolder(folder)
+	// filePath := getDotFilePath()
+	// addNewSliceElementsToFile(filePath, repositories)
+	fmt.Printf("\nSuccessfully Added!\n")
+}
+
+// scanGitFolders returns a list of subfolders of `folder` ending with `.git`.
+// Returns the base folder of the repo, the .git folder parent.
+// Recursively searches in the subfolders by passing an existing `folders` slice.
+func recursiveScanGitFolder(folder string) {
+
+	var folders []string
+
+	folder = strings.TrimSuffix(folder, "/")
+
+	f, err := os.Open(folder)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	files, err := f.Readdir(-1)
+	f.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			folders = append(folders, file.Name())
+		}
+	}
+
+	fmt.Print(folders)
+
 }
 
 // stats generates a nice graph of your Git contributions
