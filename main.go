1acfb8728b924635ffd4d7adc6447d6cf60cc708
Mohamed Gamal <mg.dev1992@gmail.com>
2020-03-28 18:49:27 +0200

    store folder paths with .git directory

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"os"
+	"os/user"
 	"strings"
 )
 
@@ -12,9 +16,9 @@ import (
 // searching for Git repositories
 func scan(folder string) {
 	fmt.Printf("Found folders:\n")
-	recursiveScanFolder(folder)
-	// filePath := getDotFilePath()
-	// addNewSliceElementsToFile(filePath, repositories)
+	repositories := recursiveScanFolder(folder)
+	filePath := getDotFilePath()
+	addNewSliceElementsToFile(filePath, repositories)
 	fmt.Printf("\nSuccessfully added\n")
 }
 
@@ -64,12 +68,93 @@ func scanGitFolders(folders []string, folder string) []string {
 }
 
 
-
 func recursiveScanFolder (folder string) []string {
 	return scanGitFolders(make([]string, 0), folder)
 }
 
+func getDotFilePath() string {
+	usr, err := user.Current()
 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dotFile := usr.HomeDir + "/.gogitlocalstats"
+	
+	return dotFile
+}
+
+
+func addNewSliceElementsToFile(filePath string, newRepos []string) {
+	existingRepos := parseFileLinesToSlice(filePath)
+    repos := joinSlices(newRepos, existingRepos)
+    dumpStringsSliceToFile(repos, filePath)
+}
+
+// given a file path string, gets the content of each line and parses it to a slice of strings.
+func parseFileLinesToSlice (filePath string) []string{
+	f := openFile(filePath)
+	defer f.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+        if err != io.EOF {
+            // panic(err)
+        }
+	}
+	
+	return lines
+}
+
+func openFile (filePath string) *os.File {
+	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY, 755)
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			_, err = os.Create(filePath)
+
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			panic(err)
+		}
+	}
+
+	return f
+}
+
+func joinSlices(new []string, existing []string) []string {
+	for _, i := range new {
+		if !sliceContains(existing, i) {
+			existing = append(existing, i)
+		}
+	}
+
+	return existing
+}
+
+func sliceContains(slice []string, value string) bool {
+	for _, v := range slice {
+		if (v == value ){
+			return true
+		}
+	}
+
+	return false
+}
+
+
+func dumpStringsSliceToFile(repos []string, filePath string) {
+	content := strings.Join(repos, "\n")
+	ioutil.WriteFile(filePath, []byte(content), 0755)
+}
+/***************Part 2************************ */
 // stats generates a nice graph of your Git contributions
 func stats(email string) {
 	print("stats")
